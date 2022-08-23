25496ed0d6cd11bd6af11c60b3365c927f33c897
GUchechi <godswilluchendu7@gmail.com>
2022-08-23 15:24:13 +0100

    added useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setSearchTerm(newValue);
+  }
+
+  const companies = [
+    "Google",
+    "Facebook",
+    "Youtube",
+    "Twitter",
+    "Linkedin",
+    "Twitch",
+    "Instagram",
+    "Whatsapp",
+  ];
+
   return (
     <div className="container">
-      <input />
+      <input value={searchTerm} onChange={handleChange} />
       <div className='data'>
         <div className='row'></div>
       </div>
